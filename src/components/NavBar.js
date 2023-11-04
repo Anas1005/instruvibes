9ba1cf6a7abb768ed41da4c310b18bed7589c7c0
Anas <anassaif.507@gmail.com>
2023-11-04 10:41:57 +0530

    added Hamburger Menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import { RiHome2Line, RiInformationLine, RiContactsLine } from "react-icons/ri";
 import { BiSearch } from "react-icons/bi";
 import { NavLink, useLocation } from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi"
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { HamburgerMenu } from "./HamburgerMenu";
 import { AppContext } from "../context/AppContext";
 import { useContext } from "react";
 
 function NavBar() {
   const [SearchItem, setSearchItem] = useState("");
+  const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
   const [isSearchActive, setIsSearchActive] = useState(false);
   const { triggerYouTubePlayPause } = useContext(AppContext);
-  const[PrevSearchItem,setPrevSearchItem]=useState('');
+  const [PrevSearchItem, setPrevSearchItem] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -19,19 +23,25 @@ function NavBar() {
   };
 
   const handleSearchBlur = (e) => {
-    if(!(e.target===document.querySelector(".BtnSubmit")||e.target===document.querySelector(".BtnSearch")||e.target===document.querySelector("input"))){
-    setIsSearchActive(false);
+    if (
+      !(
+        e.target === document.querySelector(".BtnSubmit") ||
+        e.target === document.querySelector(".BtnSearch") ||
+        e.target === document.querySelector("input")
+      )
+    ) {
+      setIsSearchActive(false);
     }
   };
 
   const handleSearchItem = (e) => {
     e.preventDefault();
-    console.log("SearchItem:"+SearchItem);
-      console.log("PrwvSearch:"+PrevSearchItem);
-      console.log("--------------------------");
-    if (SearchItem&&SearchItem!==PrevSearchItem) {
-      console.log("SearchItem:"+SearchItem);
-      console.log("PrwvSearch:"+PrevSearchItem);
+    console.log("SearchItem:" + SearchItem);
+    console.log("PrwvSearch:" + PrevSearchItem);
+    console.log("--------------------------");
+    if (SearchItem && SearchItem !== PrevSearchItem) {
+      console.log("SearchItem:" + SearchItem);
+      console.log("PrwvSearch:" + PrevSearchItem);
       setPrevSearchItem(SearchItem);
       triggerYouTubePlayPause();
       navigate(`/listen/${SearchItem.replaceAll(" ", "-")}`);
@@ -48,54 +58,138 @@ function NavBar() {
   };
 
   return (
-    <nav className="pt-[1%] md:pt-[1.5%] px-[8%] pb-[1%] w-[100vw] z-10 bg-[url('AppBG.png')] bg-contain sticky top-[0%] border-b border-[#ffffff1f] border-[1px]">
-      <div className="flex justify-between items-center Para">
-        <div className="relative flex items-center">
-          <form
-            onSubmit={handleSearchItem}
-            onClick={handleSearchClick}
-            className={`transition-all duration-1500 ${
-              isSearchActive ? "w-[50vw] md:w-[60vw]" : "w-[40px]"
-            }`}
-          >
-            <input
-              type="text"
-              placeholder={`${isSearchActive?"Search Instrumental":""}`}
-              className="p-2 pr-8 rounded-full bg-[#242424] text-white w-[100%]"
-              onBlur={handleSearchBlur}
-              onChange={changeHandler}
-              value={SearchItem}
-            />
-            <button type="submit" className="absolute BtnSubmit right-2 top-1/2 transform -translate-y-1/2 text-white">
-              <BiSearch className="w-6 h-6 BtnSearch" />
-            </button>
-          </form>
+    <div className="w-[100vw] z-10 bg-[url('AppBG.png')] bg-contain sticky top-[0%] border-b border-[#ffffff1f] border-[1px]">
+      <nav className="py-[1rem] px-[2.5rem]  w-[100vw] z-10 bg-[url('AppBG.png')] bg-contain sticky top-[0%] border-b border-[#ffffff1f] border-[1px]">
+        <div className="hidden md:flex justify-between items-center Para">
+          <div className="relative flex items-center">
+            <form
+              onSubmit={handleSearchItem}
+              onClick={handleSearchClick}
+              className={`transition-all duration-1500 ${
+                isSearchActive ? "w-[50vw] md:w-[60vw]" : "w-[40px]"
+              }`}
+            >
+              <input
+                type="text"
+                placeholder={`${isSearchActive ? "Search Instrumental" : ""}`}
+                className="p-2 pr-8 rounded-full bg-[#242424] text-white w-[100%]"
+                onBlur={handleSearchBlur}
+                onChange={changeHandler}
+                value={SearchItem}
+              />
+              <button
+                type="submit"
+                className="absolute BtnSubmit right-2 top-1/2 transform -translate-y-1/2 text-white"
+              >
+                <BiSearch className="w-6 h-6 BtnSearch" />
+              </button>
+            </form>
+          </div>
+          <ul className="flex space-x-6">
+            <li>
+              <NavLink
+                to="/instruvibes"
+                className={` text-white  hover:transition-all hover:text-[#E55857] ${
+                  isActiveRoute("/") ? "text-[#EF5350]" : ""
+                }`}
+              >
+                <RiHome2Line className="" />
+                <span className="hidden lg:block">Home</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={`text-white hover:transition-all hover:text-[#E55857] ${
+                  isActiveRoute("/about") ? "text-[#EF5350]" : ""
+                }`}
+              >
+                <RiInformationLine className="" />
+                <span className="hidden lg:block">About</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={`text-white  hover:transition-all hover:text-[#E55857] ${
+                  isActiveRoute("/contact") ? "text-[#EF5350]" : ""
+                }`}
+              >
+                <RiContactsLine className="" />
+                <span className="hidden lg:block">Contact</span>
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <ul className="flex space-x-6">
-        <li>
-        <NavLink
-          to="/instruvibes"
-          className={` text-white  hover:transition-all hover:text-[#E55857] ${isActiveRoute("/") ? "text-[#EF5350]" : ""}`}
-        >
-          <RiHome2Line className="" />
-          <span className="hidden lg:block">Home</span>
-        </NavLink>
-        </li>
-          <li>
-            <NavLink to="/about" className={`text-white hover:transition-all hover:text-[#E55857] ${isActiveRoute("/about") ? "text-[#EF5350]" : ""}`}>
-              <RiInformationLine className="" />
-              <span className="hidden lg:block">About</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" className={`text-white  hover:transition-all hover:text-[#E55857] ${isActiveRoute("/contact") ?"text-[#EF5350]" : ""}`}>
-              <RiContactsLine className="" />
-              <span className="hidden lg:block">Contact</span>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div className="mr-4 md:hidden">
+          <GiHamburgerMenu
+            onClick={() => setIsMenuModalOpen((prev) => !prev)}
+            className={` fill-richblack-100 cursor-pointer `}
+            fontSize={24}
+          />
+
+          <HamburgerMenu
+            isMenuModalOpen={isMenuModalOpen}
+            setIsMenuModalOpen={setIsMenuModalOpen}
+          >
+            <div className="flex flex-col gap-y-2 py-5 px-5">
+              <div className="relative flex items-center">
+                <form
+                  onSubmit={handleSearchItem}
+                  onClick={handleSearchClick}
+                  className={`transition-all duration-1500 ${
+                    isSearchActive ? "w-[50vw] md:w-[60vw]" : "w-[40px]"
+                  }`}
+                >
+                  <input
+                    type="text"
+                    placeholder={`${
+                      isSearchActive ? "Search Instrumental" : ""
+                    }`}
+                    className="p-2 pr-8 rounded-full bg-[#242424] text-white w-[100%]"
+                    onBlur={handleSearchBlur}
+                    onChange={changeHandler}
+                    value={SearchItem}
+                  />
+                  <button
+                    type="submit"
+                    className="absolute BtnSubmit right-2 top-1/2 transform -translate-y-1/2 text-white"
+                  >
+                    <BiSearch className="w-6 h-6 BtnSearch" />
+                  </button>
+                </form>
+              </div>
+
+              {/* General Buttons */}
+              <div className="h-[1px] my-2 bg-richblack-100 w-3/4 mx-auto"></div>
+
+
+              <Link to={"/instruvibes"} onClick={() => setIsMenuModalOpen(false)}>
+                <div className={`flex gap-x-2 items-center w-full py-2 px-3 transition-all duration-500  ${isActiveRoute("/instruvibes") ? "text-[#EF5350]" : "" }`}>
+                  <RiHome2Line className="text-lg" />
+                  Home
+                </div>
+              </Link>
+
+              <Link to={"/about"} onClick={() => setIsMenuModalOpen(false)}>
+                <div className={`flex gap-x-2 items-center w-full py-2 px-3 ransition-all duration-500  ${isActiveRoute("/about") ? "text-[#EF5350]" : "" }`}>
+                  <RiInformationLine className="text-lg" />
+                  About Us
+                </div>
+              </Link>
+
+              <Link to={"/contact"} onClick={() => setIsMenuModalOpen(false)}>
+                <div className={`flex gap-x-2 items-center w-full py-2 px-3 ransition-all duration-500  ${isActiveRoute("/contact") ? "text-[#EF5350]" : "" }`}>
+                  <RiContactsLine className="text-lg" />
+                  Contact Us
+                </div>
+              </Link>
+
+            </div>
+          </HamburgerMenu>
+        </div>
+      </nav>
+    </div>
   );
 }
 
